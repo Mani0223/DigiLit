29e631675788b042c8d040623d871aa2c707d1ff
Mani0223 <141195132+Mani0223@users.noreply.github.com>
2024-07-30 19:39:32 -0400

    Delete DigiLit directory

--- DigiLit/resources/signup.js
+++
@@ -1,7 +0,0 @@
-// Show the modal 5 seconds after the page loads
-window.addEventListener('load', function () {
-    setTimeout(function () {
-      var loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-      loginModal.show();
-    }, 5000); 
-  });
